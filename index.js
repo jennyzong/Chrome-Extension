0f67baccb7b4c21a5d7766a687d5fe49ccdf0240
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-09 20:18:05 -0400

    Chrome Extension
    - make the input button work with addEventListener()

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+// Refactor the code so that it uses .addEventListener()
+// when you click the SAVE INPUT button
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
+
+
