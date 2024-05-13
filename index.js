28a84cdf74d0241c41bc34b850e77a1576db4502
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-13 11:43:29 -0400

    chrome extension
    - use createElement() and append() instead of innerHTML

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ inputBtn.addEventListener("click", function() {
     console.log(myLeads)
 })
 
-// Replace .textContent with .innerHTML and use <li> tags
+// Let's try a different method!
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.textContent += myLeads[i] + " "
+    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // create element
+    // set text content
+    // append to ul
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
 }
 
 
