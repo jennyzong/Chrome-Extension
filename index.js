3087641535afe788949bbd32007d9a01b8705320
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-14 10:45:31 -0400

    chrome extension
    - add the <a> tag

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    renderLeads()
     inputEl.value = ""
+    renderLeads()
 })
 
-function renderLead() {
-    let listItem = "<li>" + inputEl.value + "</li>"
-    
-    ulEl.innerHTML += listItems
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    }
+    ulEl.innerHTML = listItems  
 }
-
