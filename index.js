fd5e2b119719ae6af1d0264f0468258b772d7203
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-16 11:08:27 -0400

    chrome extension
    -get the leads from localStorage

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// Get the leads from the localStorage - PS: JSON.parse()
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    // Save the myLeads array to localStorage 
-    // PS: remember JSON.stringify()
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
     
@@ -19,7 +23,7 @@ function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
-            <li>
+            <li>  
                 <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
